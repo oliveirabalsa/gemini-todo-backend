e4bc7967a349963c607c5f82e3cd4721485fc9a6
Leonardo <oliveirabalsa2@gmail.com>
2023-09-06 16:57:36 -0300

    feat(task): add enum

---
+++ src/task/types/task.enum.ts
@@ -0,0 +1,11 @@
+export enum TaskPriority {
+  HIGH = 'HIGH',
+  MEDIUM = 'MEDIUM',
+  LOW = 'LOW',
+}
+
+export enum TaskStatus {
+  TODO = 'TODO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  COMPLETED = 'COMPLETED',
+}
